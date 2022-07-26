475e223863ebe49ff255f8eb8acaff7fa0963aa8
aguswibawa <agusibawa@gmail.com>
2022-07-26 23:14:45 +0700

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import BookRouter from './src/controllers/route'
 
 const app = Express();
-const port = 3333;
+const port = 80;
 
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(bodyParser.json({limit: '30mb', extended: true}));
